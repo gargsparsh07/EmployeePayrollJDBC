3a6f41b12e31b7e034ba1eb9e67f4c88b1ad7cac
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-03-11 15:45:19 +0530

    [Sparsh] : UC2: Retrieve employee payroll data using JDBC

--- src/main/java/com/bridgelabz/EmployeePayrollMain.java
+++ src/main/java/com/bridgelabz/EmployeePayrollMain.java
@@ -1,18 +1,11 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class EmployeePayrollMain {
 
     public static void main(String[] args) {
 
-        try {
-            Connection connection = DBConnection.getConnection();
-            System.out.println("Connected to database successfully");
-        }
-        catch (SQLException e) {
-            System.out.println("Database connection failed");
-        }
+        EmployeePayrollService service = new EmployeePayrollService();
+
+        service.readEmployeeData();
     }
 }
--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,4 +1,34 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class EmployeePayrollService {
+
+    public void readEmployeeData() {
+
+        String query = "SELECT * FROM employee_payroll";
+
+        try (Connection connection = DBConnection.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+
+            while (resultSet.next()) {
+
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double salary = resultSet.getDouble("salary");
+
+                EmployeePayrollData employee =
+                        new EmployeePayrollData(id, name, salary);
+
+                System.out.println(employee);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
